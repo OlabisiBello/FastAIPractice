061ecd41ba5a680d705eaf8b1f0007405c43ed31
OlabisiBello <56558776+OlabisiBello@users.noreply.github.com>
2022-01-29 21:07:53 -0500

    Create PETS_tutorial_part_one.py

---
+++ PETS_tutorial_part_one.py
@@ -0,0 +1,16 @@
+!pip install fastai -q --upgrade
+from fastai.basics import *
+from fastai.vision.all import *
+from fastai.callback.all import *
+path = untar_data(URLs.PETS)
+path.ls()
+pets = DataBlock(
+    blocks = (ImageBlock, CategoryBlock),
+    get_items = get_image_files,
+    splitter = RandomSplitter(valid_pct = 0.2, seed= 100),
+    get_y = using_attr(RegexLabeller(pat = r'^(.*)_\d+\..*$'), 'name'),
+    item_tfms = Resize(460),
+    batch_tfms = aug_transforms(size = 224, min_scale = 0.75)
+)
+dls = pets.dataloaders(path/'images')
+dls.show_batch(max_n=4, figsize = (10,15))
